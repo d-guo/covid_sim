00602ab34c883ec448995d975469564bbc21a46c
dguo <dguo@ucsb.edu>
2020-05-27 23:41:35 -0700

    added spreading upon contact

--- animation_script.js
+++ animation_script.js
@@ -2,7 +2,7 @@ let width = window.innerWidth;
 let height = window.innerHeight;
 
 let N = 1000; // population size
-let p = 0.1 // probability of contracting disease from an encounter with an infected person
+let p = 1 // probability of contracting disease from an encounter with an infected person
 let radius = 5;
 
 let people = [];
@@ -19,8 +19,9 @@ class Person {
 function init() {
     let canvas = document.getElementById("cw");
     let ctx = canvas.getContext("2d");
-    for (let i = 0; i < N; i++) {
-        people.push(new Person(Math.random() < 0.5 ? "uninfected" : "infected", Math.floor(Math.random() * width), Math.floor(Math.random() * height)));
+    people.push(new Person("infected", Math.floor(Math.random() * width), Math.floor(Math.random() * height)));
+    for (let i = 0; i < N - 1; i++) {
+        people.push(new Person("uninfected", Math.floor(Math.random() * width), Math.floor(Math.random() * height)));
     }
     anim();
 }
@@ -40,11 +41,28 @@ function anim() {
 
     ctx.clearRect(0, 0, width, height); // clear canvas
     
+    
+    // move people
     for (let j = 0; j < N; j++) {
-        // move people
-        people[j].x += Math.floor(Math.random() * 5) - 2;
-        people[j].y += Math.floor(Math.random() * 5) - 2;
+        people[j].x += Math.floor(Math.random() * 7) - 3;
+        people[j].y += Math.floor(Math.random() * 7) - 3;
+    }
 
+    // new infections
+    for (let j = 1; j < N; j++) {
+        if(people[j].health_status == "infected") {
+            continue;
+        }
+        for (let k = 0; k < N; k++) {
+            if((Math.abs(people[k].x - people[j].x) < 5 && Math.abs(people[k].y - people[j].y) < 5) && people[k].health_status == "infected") {
+                people[j].health_status = Math.random() < p ? "infected" : "uninfected";
+                continue;
+            }
+        }
+    }
+
+    // draw new people
+    for (let j = 0; j < N; j++) {
         ctx.beginPath();
         ctx.arc(people[j].x, people[j].y, radius, 0, 2 * Math.PI, false);
         if (people[j].health_status == "uninfected") {
@@ -60,7 +78,6 @@ function anim() {
         ctx.stroke();
     }
 
-    console.log('ok');
     window.requestAnimationFrame(anim);
 }
 
